70d7acb196995eb6f14ab6312ff4bba525d3249b
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2023-10-10 18:00:54 +0200

    finished styling Dates component

--- src/Dates.js
+++ src/Dates.js
@@ -3,16 +3,16 @@ import styles from "./Dates.module.scss";
 export default function Dates() {
   return (
     <section>
-      <div className={styles.wrapper1}>
+      <div className={styles.wrapper}>
         <p>Day</p>
       </div>
-      <div className={styles.wrapper2}>
+      <div className={styles.wrapper}>
         <p>Week</p>
       </div>
-      <div className={styles.wrapper3}>
+      <div className={styles.wrapper}>
         <p>Month</p>
       </div>
-      <div className={styles.wrapper4}>
+      <div className={styles.wrapper}>
         <p>Year</p>
       </div>
     </section>
